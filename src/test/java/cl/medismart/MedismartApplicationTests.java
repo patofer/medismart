3468baf8ea4b3e6b2ea62375b4ec8bb3c439e79a
Patricio <patricio.fernandez@fedex.com>
2025-11-06 11:21:44 -0300

    nuevo proyecto

---
+++ src/test/java/cl/medismart/MedismartApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.medismart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedismartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
